b4e3f1c7d3480109a62df17e548c5a8b20726fe8
Adrian Jendo <ajendo@uwaterloo.ca>
2021-07-04 14:40:28 -0700

    Fixed graph search

--- src/Algorithms/GraphSearch.js
+++ src/Algorithms/GraphSearch.js
@@ -6,42 +6,50 @@ export const graphSearch = (startNode, nodeList, arrowList, directed, breadthFir
 
     const visited_nodes = {}; //Dictionary used to give better performance (visited_nodes[cur.id] = 1) for checking if node is visited
     //const visited_order = []; //Need to use list to get the visited_nodes order
-    const unvisited = [startNode.id];
-    const unvisited_arrows = [];
+    const queue = [startNode.id];
+    visited_nodes[startNode.id] = true;
     const animations = []; //Animations for both arrows and nodes <-- [{type:arrow/node, stage:seen/visited,id:ID}, ...]
-    
-    do {
-        //Get current node id
-        const cur_id = breadthFirstSearch ? unvisited.shift() : unvisited.pop(); //.pop for depth-first search
-        const last_adjacency = unvisited_arrows.length ? breadthFirstSearch ? unvisited_arrows.shift() : unvisited_arrows.pop() : null;
 
-        //push arrow pointing from previous node into animations
-        if(last_adjacency && !visited_nodes[last_adjacency.endID]){
-            animations.push({type:"arrow", id:last_adjacency.arrowID, nodex1:last_adjacency.nodex1, nodex2:last_adjacency.nodex2, nodey1:last_adjacency.nodey1, nodey2:last_adjacency.nodey2});
-        }
-        
-        animations.push({type:"node", id: cur_id});
-        //visited_order.push(cur_id);
-        visited_nodes[cur_id] = true; //push id of current node to visited_nodes and add animation
-        
-        for(let i = 0; i<graph[cur_id].length; ++i){ //Add unvisited nodes to list
-            const neighbour = graph[cur_id][i].endID;
-            if(!visited_nodes[neighbour] && !contains_id(neighbour, unvisited)){
-                unvisited.push(neighbour);
-                unvisited_arrows.push(graph[cur_id][i]);
+    if(breadthFirstSearch){
+        animations.push({type:"node", id: startNode.id});
+        do {
+            //Get current node id
+            const cur_id = queue.shift();
+            
+            for(let i = 0; i<graph[cur_id].length; ++i){ //Add queue nodes to list
+                const neighbour = graph[cur_id][i].endID;
+                if(!visited_nodes[neighbour]){
+                    queue.push(neighbour);
+                    const last_adjacency = graph[cur_id][i];
+                    animations.push({type:"arrow", id:last_adjacency.arrowID, nodex1:last_adjacency.nodex1, nodex2:last_adjacency.nodex2, nodey1:last_adjacency.nodey1, nodey2:last_adjacency.nodey2});
+                    animations.push({type:"node", id: neighbour});
+                    //visited_order.push(cur_id);
+                    visited_nodes[neighbour] = true; //push id of neighbour to visited_nodes and add animation
+                }
             }
-        }
-    } while(unvisited.length);
-
+        } while(queue.length);
+    }
+    else {
+        depthFirstSearch(graph, visited_nodes, startNode.id, null, animations);
+    }
+    
     return animations;
 };
 
-//Returns true if id is in arr
-const contains_id = (id, arr) => {
-    for(let i = 0; i<arr.length; ++i){
-        if(arr[i] === id){
-            return true;
+const depthFirstSearch = (graph, visited_nodes, cur_id, last_adjacency, animations) => {
+    //push arrow pointing from previous node into animations
+    if(last_adjacency && !visited_nodes[last_adjacency.endID]){
+        animations.push({type:"arrow", id:last_adjacency.arrowID, nodex1:last_adjacency.nodex1, nodex2:last_adjacency.nodex2, nodey1:last_adjacency.nodey1, nodey2:last_adjacency.nodey2});
+    }
+    
+    animations.push({type:"node", id: cur_id});
+    //visited_order.push(cur_id);
+    visited_nodes[cur_id] = true; //push id of current node to visited_nodes and add animation
+
+    for(let i = 0; i<graph[cur_id].length; ++i){ 
+        const neighbour = graph[cur_id][i].endID;
+        if(!visited_nodes[neighbour]){
+            depthFirstSearch(graph, visited_nodes, neighbour, graph[cur_id][i], animations);
         }
     }
-    return false;
-};
+}
